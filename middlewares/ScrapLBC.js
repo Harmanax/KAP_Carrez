040085d96e84ba1099c434cbfc4ae4ad67e443c7
papuech <paul.puech@devinci.fr>
2017-02-07 19:46:34 +0100

    small

--- middlewares/ScrapLBC.js
+++ middlewares/ScrapLBC.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res, next){
 
   //Under middlewares et variables locales pour tests
+  var request = require('request');
   var cheerio = require('cheerio');
   var test_MA = 'https://www.meilleursagents.com/prix-immobilier/selens-02300/';
 
