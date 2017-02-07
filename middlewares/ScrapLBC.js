ac20318978ab904b16909c4236653fa35ed2e7e6
papuech <paul.puech@devinci.fr>
2017-02-07 19:45:19 +0100

    [UP] ScrapLBC.js

--- middlewares/ScrapLBC.js
+++ middlewares/ScrapLBC.js
@@ -1,8 +1,23 @@
 module.exports = function(req, res, next){
 
+  //Under middlewares et variables locales pour tests
+  var cheerio = require('cheerio');
+  var test_MA = 'https://www.meilleursagents.com/prix-immobilier/selens-02300/';
+
+
   //ICI METTRE MODULES DE NOTRE Middleware
   req.getData = function(url){
-    console.log(url);
+    console.log("on est dans getData");
+    request(url, function(err, resp, body){
+      // DEBUT DU SCRAP
+
+      if(!err && resp.statusCode === 200){
+        var $ = cheerio.load(body);
+        $('properties', 'properties')
+      }
+
+      //FIN DU SCRAP
+    });
   }
 
 
